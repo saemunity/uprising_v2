54fe1ec8c6e97dfa6282edd3c6e6d68db4fba1b8
saem_alex <huanhoang2092000@gmail.com>
2023-07-16 09:26:45 +0700

    Created enemy two and created pickup

---
+++ Assets/Scripts/Enemies/Three/EnemyThreeController.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyThreeController : MonoBehaviour
+{
+    public float flightSpeed = 3f;
+    public float waypointReachedDistance = 0.1f;
+    public DetectionZone biteDetectionZone;
+    public Collider2D deathCollider;
+    public List<Transform> waypoints;
+
+     Animator animator;
+    Rigidbody2D rb;
+    Damageable damageable;
+
+    Transform nextWaypoint;
+    int waypointNum = 0;
+    public bool _hasTarget = false;
+
+    public bool HasTarget
+    {
+        get { return _hasTarget; }
+        set
+        {
+            _hasTarget = value;
+            animator.SetBool(AnimationStrings.hasTarget, value);
+        }
+    }
+
+    public bool CanMove
+    {
+        get { return animator.GetBool(AnimationStrings.canMove); }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        damageable = GetComponent<Damageable>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextWaypoint = waypoints[waypointNum];
+    }
+
+    // private void OnEnable()
+    // {
+    //     damageable.damageableDeath += OnDeath();
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HasTarget = biteDetectionZone.detectedColliders.Count > 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (damageable.IsAlive)
+        {
+            if (CanMove)
+            {
+                Flight();
+            }
+            else
+            {
+                rb.velocity = Vector3.zero;
+            }
+        }
+    }
+
+    private void Flight()
+    {
+        // Fly to next waypoint
+        Vector2 directionToWaypoint = (nextWaypoint.position - transform.position).normalized;
+
+        // Check if we have reacher the waypoint already
+        float distance = Vector2.Distance(nextWaypoint.position, transform.position);
+
+        rb.velocity = directionToWaypoint * flightSpeed;
+
+        UpdateDirection();
+
+        // See if we need to switch waypoints
+        if (distance <= waypointReachedDistance)
+        {
+            // Switch to next waypoint
+            waypointNum++;
+
+            if (waypointNum >= waypoints.Count)
+            {
+                // Loop back to original waypoint
+                waypointNum = 0;
+            }
+            nextWaypoint = waypoints[waypointNum];
+        }
+    }
+
+    private void UpdateDirection()
+    {
+        Vector3 localScaleVector = transform.localScale;
+
+        if (transform.localScale.x > 0)
+        {
+            // Facing the right
+            if (rb.velocity.x < 0)
+            {
+                // Flip
+                transform.localScale = new Vector3(
+                    -1 * localScaleVector.x,
+                    localScaleVector.y,
+                    localScaleVector.z
+                );
+            }
+        }
+        else
+        {
+            // Facing the left
+            if (rb.velocity.x > 0)
+            {
+                // Flip
+                transform.localScale = new Vector3(
+                    -1 * localScaleVector.x,
+                    localScaleVector.y,
+                    localScaleVector.z
+                );
+            }
+        }
+    }
+
+    public void OnDeath()
+    {
+        // Dead flier falls to the ground
+        rb.gravityScale = 2f;
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        deathCollider.enabled = true;
+    }
+}
